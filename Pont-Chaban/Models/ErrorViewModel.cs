ed698917792189905172490d9791c3335cf01b56
cicibonbec <cyrian.philippot@ynov.com>
2021-04-01 09:43:53 +0200

    création projet Pont-Chaban

---
+++ Pont-Chaban/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Pont_Chaban.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
